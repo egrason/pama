a7c458ed66711f5fcc40d65d218270781d519dee
Emily Grason <76760745+egrason@users.noreply.github.com>
2024-11-07 15:33:35 -0800

    Trapping data for annual and monthly CPUE Add sites for map table Update to final trap dataset inclusive of 2024

--- Scripts/PAMA_Distribution.R
+++ Scripts/PAMA_Distribution.R
@@ -9,7 +9,7 @@ library(lubridate) #month, day, year functions
 
 ######################################################
 ####### Data
-trap.dat <- read.csv("~/Documents/GitHub/pama/Data/TrappingData.11.4.24.eg.csv", header = T)
+trap.dat <- read.csv("~/Documents/GitHub/pama/Data/TrappingData.11.7.24.eg.csv", header = T)
 
 trap.dat$IsCompromised <- as.factor(trap.dat$IsCompromised)
 trap.dat.effort <- trap.dat[trap.dat$IsCompromised == "N",] # Remove compromised and effort only traps
@@ -19,7 +19,7 @@ trap.dat.effort <- trap.dat[trap.dat$IsCompromised == "N",] # Remove compromised
 
 #DF of total Trap sets per year per site
 effort <- aggregate(numeric(nrow(trap.dat.effort)), 
-                    trap.dat.effort[c("SiteID", "SetTime", "TrapType", "TrapNumber")], 
+                    trap.dat.effort[c("SiteID", "EndTime", "TrapType", "TrapNumber")], 
                     length) #Produces list of unique traps
 
 effort$EndTime <- as.POSIXlt(effort$EndTime, format = "%m/%d/%y %H:%M")
@@ -63,7 +63,14 @@ pama.cpue <- merge(trapsbyyear, pama,
 pama.cpue[is.na(pama.cpue)] <- 0
 pama.cpue$CPUE <- (100*pama.cpue$total.pama/pama.cpue$trap.sets)
 
-write.csv(pama.cpue, file = "Network PAMA CPUE.csv")
+
+sites <- read.csv("~/Documents/GitHub/pama/Data/Sites.csv", header = T)
+sites$SiteID <- sites$SiteNumber
+
+pama.cpue.map <- merge(pama.cpue, sites[, c("LatitudeDD", "LongitudeDD", "SiteID")],
+                   by = "SiteID", all.x = TRUE)
+
+write.csv(pama.cpue.map, file = "Network PAMA CPUE.Map.csv")
 
 ######################################################
 #######PAMA Seasonal Abundance Patterns
@@ -75,10 +82,10 @@ effort <- aggregate(numeric(nrow(trap.dat.effort)),
                     length) #Produces list of unique traps with x = number of unique species within the trap
 effort$EndTime <- as.POSIXlt(effort$EndTime, format = "%m/%d/%y %H:%M")
 effort$Year <- year(effort$EndTime)
-effort$Month <- month(effort$EndTime)
+effort$month <- month(effort$EndTime) #Derived from date and so true capture date
 
 effort$EndTime <- as.character(effort$EndTime)
-monthly.effort <- ddply(effort, c("SiteID", "Month", "Year"),
+monthly.effort <- ddply(effort, c("SiteID", "month", "Year"),
                         function(df) {
                           return(
                             c(
@@ -93,10 +100,10 @@ monthly.effort <- ddply(effort, c("SiteID", "Month", "Year"),
 trap.pama <- trap.dat.effort[trap.dat.effort$Species == "PAMA",]
 trap.pama$EndTime <- as.POSIXlt(trap.pama$EndTime, format = "%m/%d/%y %H:%M")
 trap.pama$Year <- year(trap.pama$EndTime)
-trap.pama$Month <- month(trap.pama$EndTime)
+trap.pama$month <- month(trap.pama$EndTime) #Derived from date and so true capture date
 trap.pama$EndTime <- as.character(trap.pama$EndTime)
 
-pama.month <- ddply(trap.pama, c("SiteID", "Month", "Year"),
+pama.month <- ddply(trap.pama, c("SiteID", "month", "Year"),
               function(df) {
                 return(
                   c(
@@ -113,4 +120,17 @@ pama.month.CPUE <- merge(monthly.effort, pama.month,
 
 pama.month.CPUE[is.na(pama.month.CPUE)] <- 0
 pama.month.CPUE$CPUE <- (100*pama.month.CPUE$total.pama/pama.month.CPUE$trap.sets)
+
+
+pama.month.367 <- pama.month.CPUE[pama.month.CPUE$SiteID == "367", ]
+pama.month.362 <- pama.month.CPUE[pama.month.CPUE$SiteID == "362", ]
+pama.month.378 <- pama.month.CPUE[pama.month.CPUE$SiteID == "378", ]
+pama.month.529 <- pama.month.CPUE[pama.month.CPUE$SiteID == "529", ]
+pama.month.528 <- pama.month.CPUE[pama.month.CPUE$SiteID == "528", ]
+pama.month.386 <- pama.month.CPUE[pama.month.CPUE$SiteID == "378", ]
+pama.month.599 <- pama.month.CPUE[pama.month.CPUE$SiteID == "599", ]
+pama.month.516 <- pama.month.CPUE[pama.month.CPUE$SiteID == "516", ]
+pama.month.133 <- pama.month.CPUE[pama.month.CPUE$SiteID == "133", ]
+
+
 
